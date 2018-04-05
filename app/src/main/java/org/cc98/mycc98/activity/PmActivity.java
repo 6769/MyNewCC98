e9a04fd8eb721d042309584610715822f627b008
5pipi <5pipitk@gmail.com>
2018-04-05 19:06:45 +0800

    make up for PMlist

--- app/src/main/java/org/cc98/mycc98/activity/PmActivity.java
+++ app/src/main/java/org/cc98/mycc98/activity/PmActivity.java
@@ -2,10 +2,11 @@ package org.cc98.mycc98.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
 
 import org.cc98.mycc98.R;
 import org.cc98.mycc98.activity.base.BaseSwipeBackActivity;
@@ -13,12 +14,22 @@ import org.cc98.mycc98.activity.base.BaseSwipeBackActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+/*
+* Receive all four types of messages, represents here.
+* */
+
 public class PmActivity extends BaseSwipeBackActivity {
-    public static void startActivity(Context context){
-        Intent intent= new Intent(context,PmActivity.class);
+
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, PmActivity.class);
         context.startActivity(intent);
     }
 
+    @BindView(R.id.activity_pm_view_pager_magic_indicator)
+    MagicIndicator activityPmViewPagerMagicIndicator;
+    @BindView(R.id.activity_pm_view_pager)
+    ViewPager activityPmViewPager;
     @BindView(R.id.activity_pm_toolbar)
     Toolbar toolbar;
 
---
+++ app/src/main/java/org/cc98/mycc98/activity/PmChatsActivity.java
@@ -0,0 +1,20 @@
+package org.cc98.mycc98.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.cc98.mycc98.R;
+import org.cc98.mycc98.activity.base.BaseSwipeBackActivity;
+
+public class PmChatsActivity extends BaseSwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pm_chats);
+
+
+
+
+    }
+}
